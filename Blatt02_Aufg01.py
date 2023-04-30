6ed89ecfcdd0588c9117ae204b16161013d94043
Mynna <my2020anna@gmail.com>
2023-04-30 14:13:57 +0200

    Aufg bis 1.2 gelöst

--- Blatt02_Aufg01.py
+++ Blatt02_Aufg01.py
@@ -2,6 +2,7 @@ import numpy as np
 from skimage.io import imread,imsave
 from matplotlib import pyplot as plt
 
+##Aufgabe 1.1
 img=imread("./astronaut.png")
 plt.imshow(img, cmap="gray", vmin=0, vmax=255)
 
@@ -14,8 +15,23 @@ imsave("./astroModified.png",black)
 
 plt.imshow(black, cmap="gray", vmin=0, vmax=255)
 
+#plt.show()
+
+##Aufgabe 1.2
+imagecopy1=img.copy() #Nur Fotokopien lassen sich bearbeiten
+imagecopy1[10:50,105:130] =0
+
+#Wenn eine Arrayreihe bzw. Pixel auf 0 gesetzt wird,
+#erkennt man Unterschied nicht
+
+#Helm wird geschwärzt
+imagecopy2=img.copy()
+imagecopy2[330:520,250:550]=0
+
+plt.imshow(imagecopy2, cmap="gray", vmin=0, vmax=255)
+
+#print(img.shape)
 
 plt.show()
-
 
 
