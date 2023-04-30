a7ea6329480e9d34421b369f2c280dce8f93f637
Mynna <my2020anna@gmail.com>
2023-04-30 10:52:47 +0200

    showing picture from local

--- Blatt02_Aufg01.py
+++ Blatt02_Aufg01.py
@@ -1,5 +1,11 @@
-import numpy
-import matplotlib
-import skimage.io
+import numpy as np
+from skimage.io import imread,imsave
+from matplotlib import pyplot as plt
+
+img=imread("./astronaut.png")
+plt.imshow(img, cmap="gray", vmin=0, vmax=255)
+
+plt.show()
+
 
 
