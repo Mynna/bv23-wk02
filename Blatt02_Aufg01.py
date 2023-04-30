61b1ad4468e52b53b839bb3a162d3a528ed35ff7
Mynna <my2020anna@gmail.com>
2023-04-30 11:29:43 +0200

    Brosche cut

--- Blatt02_Aufg01.py
+++ Blatt02_Aufg01.py
@@ -5,6 +5,16 @@ from matplotlib import pyplot as plt
 img=imread("./astronaut.png")
 plt.imshow(img, cmap="gray", vmin=0, vmax=255)
 
+#print(type(img))
+#print("My info: "+ str(img.dtype))
+
+black=img[340:430,120:220]
+
+imsave("./astroModified.png",black)
+
+plt.imshow(black, cmap="gray", vmin=0, vmax=255)
+
+
 plt.show()
 
 
